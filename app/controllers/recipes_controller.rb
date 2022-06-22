f0fa57c3114385eec08c4bab8359972d835883eb
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-06-22 22:37:22 +0200

    Created a `Recipe List` ..

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,31 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.all
+  end
 
-  def show; end
+  # def show
+  #   @recipe = Recipe.find(:params[id])
+  # end
+
+  def new; end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path, notice: 'Recipe created successfully!'
+    else
+      render new, status: :unprocessable_entity
+    end
+  end
 
   def public_recipes
     @public_recipes = Recipe.includes(:user).all.where(public: true).order(created_at: :desc)
     render :public_recipes
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'recipes#index'
   get 'public_recipes', to: 'recipes#public_recipes', as: 'public_recipes_action'
   
-  resources :recipes, only: [:index, :show]
+  resources :recipes, only: [:index, :show, :create, :new]
   resources :foods, only: [:index, :show, :create, :new]
   
   get 'general_shopping_list', to: 'user#general_shopping_list', as: 'shopping_list_action'

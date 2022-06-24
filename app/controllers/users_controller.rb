a61100c01492edf3577f7262e786c5ec2e6b4677
backtofayton <yucelolcay@gmail.com>
2022-06-25 01:53:07 +0300

    fix users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def general_shopping_list
-    @user = User.includes(:recipes).find(2)
+    @user = current_user
 
     (@required_foods_list, @required_foods_in_users_recipes,
     @required_foods_in_users_inventory, @cost_of_missing_foods_in_recipes) = @user.required_foods_data

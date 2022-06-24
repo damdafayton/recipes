843291fbedf13e2e8e26c4eeeba9e89db1d336c4
backtofayton <yucelolcay@gmail.com>
2022-06-24 15:49:12 +0300

    Add method to food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,4 +5,20 @@ class FoodsController < ApplicationController
   end
 
   def new; end
+
+  def create
+    new_food = Food.new(create_params)
+    if new_food.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def create_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
+  
 end

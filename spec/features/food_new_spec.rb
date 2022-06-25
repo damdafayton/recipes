d9dac5620434875c283d7b7c958714f3e3aca001
backtofayton <yucelolcay@gmail.com>
2022-06-25 14:15:01 +0300

    Linter

--- spec/features/food_new_spec.rb
+++ spec/features/food_new_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'New food page,' do
 
   describe 'redirects' do
     it 'to login' do
-      expect(page).to have_content("You need to sign in or sign up before continuing")
+      expect(page).to have_content('You need to sign in or sign up before continuing')
     end
   end
 end
